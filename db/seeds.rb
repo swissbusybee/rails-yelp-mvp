cd565c491037e8a36700620ea4c5ee788238f62c
Sue Rocha <swissbusybee@gmail.com>
2020-04-16 11:33:13 +0200

    scaffold restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+restaurants_attributes = [
+  {
+    name: "Chipotle",
+    address: "Dallas, Texas",
+    phone_number: "(214)534-1293",
+    category: "belgian"
+  },
+  {
+    name: "Olive Garden",
+    address: "San Antonio, Texas",
+    phone_number: "(216)456-1298",
+    category: "italian"
+  },
+  {
+    name: "Chinese Chicken",
+    address: "Atlanta, Georgia",
+    phone_number: "(404)765-8765",
+    category: "chinese"
+  },
+  { name: "Le Madeliene",
+    address: "Dallas, Texas",
+    phone_number: "(469)124-3457",
+    category: "french"
+  },
+  { name: "Yoogis",
+    address: "Zurich, Switzerland",
+    phone_number: "+41 456 1298 667",
+    category: "japanese"
+  }
+]
+
+  Restaurant.create!(restaurants_attributes)
+  puts "Created #{restaurant.name}"
+
+  puts "Finished!"
